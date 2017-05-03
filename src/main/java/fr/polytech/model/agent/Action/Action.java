99026d008e88dd733dfeeb874cd0e6b66129c975
agory <gory.alexandre02@gmail.com>
2017-05-03 10:17:13 +0200

    agent action

---
+++ src/main/java/fr/polytech/model/agent/Action/Action.java
@@ -0,0 +1,7 @@
+package fr.polytech.model.agent.Action;
+
+/**
+ * Created by gorya on 03/05/2017.
+ */
+public class Action {
+}
--- src/main/java/fr/polytech/model/agent/AgentRandom.java
+++ src/main/java/fr/polytech/model/agent/AgentRandom.java
@@ -1,7 +1,11 @@
 package fr.polytech.model;
 
+import java.awt.event.ActionListener;
+
 /**
  * Created by gorya on 03/05/2017.
  */
 public class AgentRandom {
+    private ActionListener listener;
+
 }
